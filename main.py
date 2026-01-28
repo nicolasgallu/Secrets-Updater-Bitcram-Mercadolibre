10348d419aba25f2b9408ce7ea230ad17b103486
nicolasgallu <nicolasgallu@pm.me>
2026-01-28 00:36:44 -0300

    testing prints cause logs dont show

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mercadolibre_secrets import updating_meli_secrets
 from bitcram_secrets import updating_bitcram_secrets
 
-
+print("comenzandon con bit")
 updating_bitcram_secrets()
+print("comenzandon con meli")
 updating_meli_secrets()
