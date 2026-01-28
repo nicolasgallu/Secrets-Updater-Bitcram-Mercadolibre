f04472d5fc928c93c01f794b37c53c2912f7e799
nicolasgallu <nicolasgallu@pm.me>
2026-01-28 00:30:03 -0300

    adding sys.stdout to write logs in gcp jobs

--- logger.py
+++ logger.py
@@ -1,9 +1,11 @@
 import logging
+import sys
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s / %(module)s / %(levelname)s / %(message)s',
-    datefmt='%Y-%m-%d %I:%M:%S'
+    datefmt='%Y-%m-%d %I:%M:%S',
+    stream=sys.stdout
 
 )
 
