9d77d80326c702281f8a7b276a047bc1a6bd4b2d
nicolasgallu <nicolasgallu@pm.me>
2026-02-03 19:55:02 -0300

    si el token de bitcram no estaba creado fallaba, ahora puede correr el job sin necesidad de levantar primero el token

--- bitcram_secrets.py
+++ bitcram_secrets.py
@@ -5,37 +5,42 @@ from google.cloud import secretmanager
 from notifications import enviar_mensaje_whapi
 from settings import PROJECT_ID, SECRET_BITCRAM_ID, USER_BITCRAM, PASSWRD_BITCRAM, TOKEN_WHAPI, PHONE
 
+def renovate_token(client):
+    
+    logger.info("Renovating Token")
+    response = requests.post(
+        "https://app.pos.bitcran.com/api/auth/",
+        json={"username": USER_BITCRAM, "password": PASSWRD_BITCRAM}
+    )
+    response.raise_for_status()
+    token = response.json()["token"]
+    client.add_secret_version(
+        parent=f"projects/{PROJECT_ID}/secrets/{SECRET_BITCRAM_ID}",
+        payload={"data": token.encode("UTF-8")}
+    )
+    logger.info("Token Renovated Succesfully")
+
+
 def updating_bitcram_secrets():
 
     try:
         client = secretmanager.SecretManagerServiceClient()
         name = f"projects/{PROJECT_ID}/secrets/{SECRET_BITCRAM_ID}/versions/latest"
-        response = client.access_secret_version(request={"name": name})
-        version = client.get_secret_version(request={"name": name})  
+        try:
+            version = client.get_secret_version(request={"name": name})  
+            #check vencimiento
+            fecha_creacion = version.create_time 
+            ahora = datetime.datetime.now(datetime.timezone.utc)
+            antiguedad = ahora - fecha_creacion
+            if antiguedad.days < 5:
+                logger.info("Token still inside the time window of 5 days")
+                return  None
+            else:
+                renovate_token(client)
 
-        #check vencimiento
-        fecha_creacion = version.create_time 
-        ahora = datetime.datetime.now(datetime.timezone.utc)
-        antiguedad = ahora - fecha_creacion
-        if antiguedad.days < 5:
-            logger.info("Token still inside the time window of 5 days")
-            return  None
-        else:
-            logger.info("Renovating Token")
+        except:
+            renovate_token(client)
 
-            response = requests.post(
-                "https://app.pos.bitcran.com/api/auth/",
-                json={"username": USER_BITCRAM, "password": PASSWRD_BITCRAM}
-            )
-
-            response.raise_for_status()
-            token = response.json()["token"]
-            client.add_secret_version(
-                parent=f"projects/{PROJECT_ID}/secrets/{SECRET_BITCRAM_ID}",
-                payload={"data": token.encode("UTF-8")}
-            )
-            logger.info("Token Renovated Succesfully")
-            
     except Exception as e:
         logger.info("token renovation failed")
         message = f"""Fallo en la renovacion del token de Bitcram el error fue: {e}"""
